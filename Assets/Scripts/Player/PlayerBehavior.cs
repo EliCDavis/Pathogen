fa5aa1c74ec5b3cc6b4476883340a9c527b3ac69
Eli Davis <eli.davis1995@gmail.com>
2017-03-05 04:21:44 -0600

    Cells can now be shot at and destroyed

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -63,7 +63,7 @@ namespace Pathogen.Player {
 		}
 
 		void InputUpdate() {
-			turn (new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), veinCenter, maxDistanceFromVeinCenter);
+			turn (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), veinCenter, maxDistanceFromVeinCenter);
 
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				Shoot ();
@@ -128,7 +128,7 @@ namespace Pathogen.Player {
 		/// </summary>
 		private void Shoot(){
 			GameObject bullet = Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
-			bullet.GetComponent<Rigidbody> ().AddForce (bulletSpawn.transform.forward*30, ForceMode.Impulse);
+			bullet.GetComponent<Rigidbody> ().AddForce (bulletSpawn.transform.forward*50, ForceMode.Impulse);
 		}
 
 		/// <summary>
--- Assets/Scripts/Scene/Veins/GameBehavior.cs
+++ Assets/Scripts/Scene/Veins/GameBehavior.cs
@@ -21,6 +21,10 @@ namespace Pathogen.Scene.Veins {
 		[SerializeField]
 		private AudioSource heartBeatClip;
 
+		private float redCellSpawnPeriod = 0.1f;
+
+		private List<GameObject> redCellsInScene;
+
 		List<GameObject> veinsSpawned;
 
 		GameObject playerInstance;
@@ -32,10 +36,12 @@ namespace Pathogen.Scene.Veins {
 		}
 
 		private void StartGame() {
+			redCellsInScene = new List<GameObject> ();
 			currentState = GameState.Started;
 			playerInstance = LoadPlayer ();
 			playerInstance.transform.position = Vector3.zero;
 			StartCoroutine (SpawnVeinSections ());
+			StartCoroutine (SpawnBloodCells());
 		}
 
 		private GameObject LoadPlayer() {
@@ -60,12 +66,38 @@ namespace Pathogen.Scene.Veins {
 		}
 
 
+		/// <summary>
+		/// Animates the heartbeat in a seperate thread
+		/// </summary>
+		/// <returns>The heartbeat.</returns>
 		private IEnumerator AnimateHeartbeat(){
 			while (heartBeatClip != null) {
 				yield return new WaitForSeconds(.1f);
 				if (!heartBeatClip.isPlaying && Mathf.Max (0, (Mathf.Sin (Time.time * 4) * 2) - (Mathf.Pow (Mathf.Sin (Time.time * 4), 4) / .8f)) > 1) {
 					heartBeatClip.Play ();
 				}
+			}
+		}
+
+		private IEnumerator SpawnBloodCells(){
+			while (currentState != GameState.Ended && playerInstance != null) {
+
+				// Spawn a cell
+				GameObject cell = CellFactory.CreateRedBloodCell (new Vector3(Random.Range(-9, 9), Random.Range(-10, 8), veinsSpawned.Count*27));
+				cell.GetComponent<Rigidbody> ().AddForce (Vector3.back*20, ForceMode.Impulse);
+				redCellsInScene.Add (cell);
+
+				// Clean up cells the player has passed or destroyed
+				for (var i = redCellsInScene.Count - 1; i >= 0; i--) {
+					if (redCellsInScene[i] == null || redCellsInScene[i].transform.position.z + 100 < playerInstance.transform.position.z) {
+						Destroy (redCellsInScene[i]);
+						redCellsInScene.Remove (redCellsInScene[i]);
+					}
+				}
+
+				// Wait till next time to spawn
+				yield return new WaitForSeconds(redCellSpawnPeriod);
+
 			}
 		}
 
